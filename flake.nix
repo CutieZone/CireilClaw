afe26f06bf9310a8e9ec5692cc5866a1c54d69db
Lyssieth <lys@the.cutie.zone>
2026-02-08 10:50:45 +0200

    chore: ensure package manager independence

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs_latest
+            pnpm
+            bubblewrap
+          ];
+        };
+      }
+    );
+}
