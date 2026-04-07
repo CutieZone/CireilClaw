fe9c12c27dbd8371b34e11eaad8f609e8a8930d6
Lyssieth <lys@the.cutie.zone>
2026-04-08 00:46:16 +0300

    fix: ApiKey schema fix

--- src/config/schemas/shared.ts
+++ src/config/schemas/shared.ts
@@ -3,7 +3,10 @@ import * as vb from "valibot";
 const nonEmptyString = vb.pipe(vb.string(), vb.nonEmpty(), vb.description("a non-empty string"));
 
 const ApiKeySchema = vb.pipe(
-  vb.union([nonEmptyString, vb.pipe(vb.array(nonEmptyString), vb.minLength(1))]),
+  vb.exactOptional(
+    vb.union([nonEmptyString, vb.pipe(vb.array(nonEmptyString), vb.minLength(1))]),
+    "not-needed",
+  ),
   vb.description("An API key, or an array of API keys"),
 );
 
